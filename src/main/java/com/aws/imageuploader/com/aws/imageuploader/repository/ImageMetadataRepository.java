b32ba9599cba8c3ebf7f87fe5006209c5700b5e6
aditee99 <aditeeadhikari98@gmail.com>
2024-06-17 21:25:21 +0530

    Revert "image uploader first commit"
    This reverts commit 2f051e6ca6fdc3a36665667bc85f7d949a4532f8.

--- src/main/java/com/aws/imageuploader/com/aws/imageuploader/repository/ImageMetadataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aws.imageuploader.com.aws.imageuploader.repository;
-
-
-import com.aws.imageuploader.com.aws.imageuploader.entity.ImageMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageMetadataRepository extends JpaRepository<ImageMetadata, Long> {
-}
-
--- src/main/java/com/aws/imageuploader/com/aws/imageuploader/service/ImageUploadException.java
+++
@@ -1,9 +0,0 @@
-package com.aws.imageuploader.com.aws.imageuploader.service;
-
-import java.io.IOException;
-
-public class ImageUploadException extends RuntimeException {
-    public ImageUploadException(String message) {
-        super(message);
-    }
-}
